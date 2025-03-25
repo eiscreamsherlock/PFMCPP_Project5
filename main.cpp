4d3c613bb5753d66395ca933f443fa220a2ccfa0
David <shuler.david.p+github@gmail.com>
2025-03-25 17:22:04 +0000

    copying code from 3.5, adding initial constructors and destructors

--- main.cpp
+++ main.cpp
@@ -70,29 +70,436 @@ write 3 UDTs below that EACH have:
  You will use those in Part 3 of this project.
 
  */
-
+#include <iostream>
 /*
  copied UDT 1:
  */
+struct FountainPen
+{
+    FountainPen();
+    ~FountainPen();
+    std::string inkColor = "black";
+    float inkRemaining, nibWidthInMm, feedCapacity;
+    int daysSinceLastClean;
+
+    struct Nib
+    {
+        Nib();
+        ~Nib();
+        std::string style = "extra fine";
+        bool  needsCleaning, isPolished; 
+        int lengthInMm = 25;
+        double mmDistanceBetweenTines = 0.001;
+
+        void cleanNib(bool toClean);
+        void polishNib(std::string nibStyle, bool isPolished);
+        void changeNib(std::string newNib);
+        int grindNib(int amntToGrnd);
+    };
+
+    Nib currentInstalledNib;
+
+    void writeCharacter(char userCharacter);
+    void drawALine(int x_start, int y_start, int lengthOfLine);
+    int getMmNibLength(Nib currentNib);
+    int compareNibLength(Nib currentNib, Nib newNib);
+};
+
+FountainPen::FountainPen() :
+inkRemaining(30.0f),
+nibWidthInMm(10.5f),
+feedCapacity(30.0f)
+{
+    daysSinceLastClean = 30;
+    std::cout << "FountainPen is being constructed!\n";
+}
+
+FountainPen::~FountainPen()
+{
+    std::cout << "FountainPen is now destructorating.\n";
+}
+
+FountainPen::Nib::Nib() :
+needsCleaning(false),        // New nibs are hopefully clean by default
+isPolished(false)            // but not necessarily polished
+{
+    std::cout << "Nib is being constructed!\n";
+}
+
+FountainPen::Nib::~Nib()
+{
+    std::cout << "Your nib is destroyed!\n";
+}
+void FountainPen::Nib::cleanNib(bool needClean)
+{
+    if (needClean)
+    {
+        std::cout << "All clean!\n";
+        needsCleaning = false;
+    }
+    else
+    {
+        std::cout << "Nothing to do, already clean!\n";
+    }
+}
+
+void FountainPen::Nib::polishNib(std::string nStyle, bool alreadyPolished)
+{
+    if (alreadyPolished)
+    {
+        std::cout << "Already shiney and smooth.  Time to get writing!\n";
+    }
+    else
+    {
+        std::cout << "Let's get that " << nStyle << "nib ready for the page...\n";
+        isPolished = true;
+        std::cout << "All done!\n";
+    }
+}
+
+void FountainPen::Nib::changeNib(std::string newNib)
+{
+    style = newNib;
+}
+
+int FountainPen::Nib::grindNib(int amnt)
+{
+    for(int i = 0; i < amnt; ++i)
+    {
+        if(lengthInMm <= 0)
+        {
+            std::cout << "We've run out of nib to grind. Better get a new one...\n";
+            return lengthInMm;
+        }
+        std::cout << "Grinding... Pass: " << i+1 << std::endl;
+        lengthInMm--;
+    }
+    std::cout << "Successfully removed " << amnt << "mm from " << style << std::endl;
+    std::cout << style << " is now " << lengthInMm << "mm long.\n";
+    return lengthInMm;
+}
+
+void FountainPen::writeCharacter(char userChar)
+{
+    std::cout << userChar << std::endl;
+}
+
+void FountainPen::drawALine(int x_start, int y_start, int lineLeng)
+{
+    std::cout << x_start << " x ";
+    int i{};
+    while (++i < lineLeng)
+    {
+        std::cout << "-";
+    }
+    std::cout << " y " << y_start << "\n";
+}
+
+int FountainPen::getMmNibLength(Nib currentNib)
+{
+    return currentNib.lengthInMm;
+}
+
+int FountainPen::compareNibLength(Nib currentNib, Nib newNib)
+{
+    if (currentNib.lengthInMm < newNib.lengthInMm)
+    {
+        std::cout << "The new nib is longer.\n";
+        return newNib.lengthInMm;
+    }
+    std::cout << "The current nib is longer.\n";
+    return currentNib.lengthInMm;
+}
+
 
 /*
  copied UDT 2:
  */
+struct CdChanger
+{
+    CdChanger();
+    ~CdChanger();
+    int numDiscsInChanger, maxNumDiscsInChanger, currTrackNumber, currentDiscNumber;
+    std::string currentTrackName;
+    std::string currentDisc;
 
+    struct Disc
+    {
+        Disc();
+        ~Disc();
+        std::string albumName, firstTrack;
+        int numOfTracks = 12;            // I know this limits all discs to 12 tracks.
+        std::string trackList[12] {};    // but I don't know how to do a dynamic-sized array in C++ yet
+
+        void populateAlbumTrackNames();
+        void displayAlbumName();
+    };
+
+    Disc activeDisk;
+
+    void playCD(int newCdNumber, Disc discToPlay);
+    void playCdTillEnd(int startingTrack);
+    void changeTrack (int newTrackNumber, Disc currentDisc);
+    void pausePlayback();
+
+};
+
+CdChanger::Disc::Disc()
+{
+    albumName = "Songs from the Aqualounge";
+    firstTrack = "Temporary High";
+    std::cout << "Disc is being constructed!\n";
+}
+
+CdChanger::Disc::~Disc()
+{
+    std::cout << "You're an echte Gangster.  You've burned your CD.\n";
+}
+
+void CdChanger::Disc::displayAlbumName()
+{
+    std::cout << "This album is called: " << albumName << std::endl;
+}
+
+void CdChanger::Disc::populateAlbumTrackNames()
+{
+    int i{};
+    while(i < numOfTracks)
+    {
+        std::string thisTrack = std::to_string(i);
+        trackList[i] = "Track No. " + thisTrack;
+        ++i;
+    }
+}
+
+CdChanger::CdChanger() :
+numDiscsInChanger(0),
+maxNumDiscsInChanger(32),
+currTrackNumber(1),
+currentDiscNumber(1)
+{
+    currentTrackName = "Sister Christian";
+    currentDisc = "Midnight Madness";
+    std::cout << "CdChanger is being constructed!\n";
+}
+
+CdChanger::~CdChanger()
+{
+    std::cout << "Your changer of CDs has now asplode.\n";
+}
+
+void CdChanger::playCD(int newCdNum, Disc disc)
+{
+    currentDisc = disc.albumName;
+    currentDiscNumber = newCdNum;
+    currentTrackName = disc.firstTrack;
+    currTrackNumber = 1;
+}
+
+void CdChanger::playCdTillEnd(int start)
+{
+    int i = start;
+    while(i <= activeDisk.numOfTracks)
+    {
+        std::cout << "Now playing track: " << i << std::endl;
+        ++i;
+    }
+    currTrackNumber = 1;        // Reset to start after finishing the disk
+}
+
+void CdChanger::changeTrack (int newTrackNum, Disc currDisc)
+{
+    currTrackNumber = newTrackNum;
+    currentTrackName = currDisc.trackList[newTrackNum];
+}
+void CdChanger::pausePlayback()
+{
+    std::cout << "*SOUND OF SILENCE*\n";
+}
 /*
  copied UDT 3:
  */
+struct GameBoy
+{
+    GameBoy();
+    ~GameBoy();
+    int screenSize, batteryPercentageRemain;
+    double screenBrightness = 75.0;
+    std::string nameOfInsertedGame = "Pokemon Yellow";
+    float volume; 
+
+    struct Cartridge
+    {
+        Cartridge();
+        ~Cartridge();
+        std::string name = "Battle Toads";
+        std::string cartridgeColor = "grey";
+        bool isRumblePackEnabled, isBattleToads, isClean;
+        int maxMemory, memoryUsed, currentLvl, maxLvl;
+
+        void saveGameStateToRAM();
+        void cleanCartridgeHead(Cartridge cName);
+        bool doYouHaveBattleToads();
+        void playTilLvl(int tgtLvl);
+    };
+
+    Cartridge currentGame;
+
+    void howBigIsScreen();
+    float adjustVolume(float adjustAmount);
+    double adjustBrightness(double adjustAmount);
+    void insertNewCartridge(Cartridge newGame);
+    void playGameTilOver();
+};
+
+GameBoy::Cartridge::Cartridge() :
+isRumblePackEnabled(false),
+isBattleToads(false),
+maxMemory(16),
+memoryUsed(7),
+currentLvl(1),
+maxLvl(13)
+{
+    isClean = true;
+    std::cout << "Cartridge is being constructed!\n";
+}
+
+GameBoy::Cartridge::~Cartridge()
+{
+    std::cout << "Shoulda called GameStop while you had the chance.  This cartridge is now destroyed.\n";
+}
+    
+GameBoy::GameBoy() :
+batteryPercentageRemain(25),
+volume(15.0f)
+{
+    screenSize = 5;
+    std::cout << "GameBoy is being constructed!\n";
+}
+
+GameBoy::~GameBoy()
+{
+    std::cout << "No more portable gaming for you.  GameBoy is now destroyed.\n";
+}
+
+float GameBoy::adjustVolume(float adjAmount)
+{
+    return volume += adjAmount;
+}
+
+double GameBoy::adjustBrightness(double adjAmount)
+{
+    return screenBrightness += adjAmount;
+}
+
+void GameBoy::insertNewCartridge(Cartridge nGame)
+{
+    currentGame = nGame;
+}
+
+void GameBoy::howBigIsScreen()
+{
+    std::cout << "This Gameboy has a " << screenSize << " sized screen.\n";
+}
+
+void::GameBoy::playGameTilOver()
+{
+    while (--batteryPercentageRemain > 0)
+    {
+        std::cout << "Doin some serious gaming!\n";
+    }
+    std::cout << "Powering dow-\n";
+}
+
+void GameBoy::Cartridge::saveGameStateToRAM()
+{
+    if (memoryUsed < maxMemory)
+    {
+        std::cout << "You have saved the game!\n";
+        maxMemory += 1;
+    }
+    else
+    {
+        std::cout << "There is not space left for a new save...\n";
+    }
+
+}
+
+void GameBoy::Cartridge::cleanCartridgeHead(Cartridge cN)
+{
+    if (cN.isClean)
+    {
+        std::cout << "Save your breath, this one's already clean!\n";
+    }
+    else
+    {
+        std::cout << "*wind noises...*\n";
+        isClean = true;
+        std::cout << cN.name << " is all clean!\n";
+    }
+}
+
+bool GameBoy::Cartridge::doYouHaveBattleToads()
+{
+    if (isBattleToads) return true;
+
+    return false;
+}
+
+void GameBoy::Cartridge::playTilLvl(int tLvl)
+{
+    while (currentLvl < tLvl)
+    {
+        if (currentLvl >  maxLvl)
+        {
+            std::cout << "Max level reached!  Congratulations on finishing the game!\n";
+            break;
+        }
+        std::cout << "Now playing level " << currentLvl << "...\n";
+        std::cout << "Level complete!\n";
+        ++currentLvl;
+    }
+
+}
 
 /*
  new UDT 4:
  with 2 member functions
  */
+struct PenCase
+{
+    PenCase();
+    ~PenCase();
+};
 
+PenCase::PenCase()
+{
+    std::cout << "A new pencase has been created!\n";
+}
+    
+PenCase::~PenCase()
+{
+    std::cout << "You'll need to find a new case to your pens in...\n";
+}
 /*
  new UDT 5:
  with 2 member functions
  */
-
+struct RoadTripEntertainment
+{
+    RoadTripEntertainment();
+    ~RoadTripEntertainment();
+};
+    
+RoadTripEntertainment::RoadTripEntertainment()
+{
+    std::cout << "Road Trip Entertainment is ready to go!\n";
+}
+    
+RoadTripEntertainment::~RoadTripEntertainment()
+{
+    std::cout << "There will be no further entertainment on this roadtrip...\n";
+}
 /*
  MAKE SURE YOU ARE NOT ON THE MASTER BRANCH
 
@@ -107,8 +514,95 @@ write 3 UDTs below that EACH have:
  Wait for my code review.
  */
 
-#include <iostream>
+//#include <iostream>    //moving this include above to resolve some error messages
 int main()
 {
+    FountainPen platinum;
+    std::cout << "--------------------------------------------------------------------\n";
+
+    FountainPen::Nib flexyGold;
+    flexyGold.style = "gold flexible";
+    std::cout << "--------------------------------------------------------------------\n";
+
+    FountainPen::Nib music;
+    music.style = "music";
+    std::cout << "--------------------------------------------------------------------\n";
+
+    GameBoy gbColor;
+    std::cout << "--------------------------------------------------------------------\n";
+
+    GameBoy::Cartridge goldenSun;
+    goldenSun.name = "Golden Sun";
+    std::cout << "--------------------------------------------------------------------\n";
+
+    GameBoy::Cartridge marioBros3;
+    marioBros3.name = "Mario Bros 3";
+    std::cout << "--------------------------------------------------------------------\n";
+    
+    CdChanger phillips;
+    std::cout << "--------------------------------------------------------------------\n";
+
+    CdChanger::Disc vanHalen;
+    vanHalen.albumName = "Van Halen";
+    std::cout << "--------------------------------------------------------------------\n";
+
+    CdChanger::Disc rideTheLightning;
+    rideTheLightning.albumName = "Ride The Lightning";
+    rideTheLightning.populateAlbumTrackNames();
+    std::cout << "--------------------------------------------------------------------\n";
+
+    CdChanger::Disc imagesAndWords;
+    imagesAndWords.albumName = "Images And Words";
+    imagesAndWords.populateAlbumTrackNames();
+    std::cout << "--------------------------------------------------------------------\n";
+
+    CdChanger::Disc bustinOut;
+    bustinOut.albumName = "Bustin Out";
+    bustinOut.populateAlbumTrackNames();
+    std::cout << "--------------------------------------------------------------------\n";
+
+    CdChanger::Disc kindOfBlue;
+    kindOfBlue.albumName = "Kind Of Blue";
+    kindOfBlue.populateAlbumTrackNames();
+    std::cout << "--------------------------------------------------------------------\n";
+
+    platinum.writeCharacter('q');
+    platinum.drawALine(1, 5, 15);
+    platinum.getMmNibLength(flexyGold);
+    platinum.compareNibLength(platinum.currentInstalledNib, music);
+    music.grindNib(5);
+    flexyGold.grindNib(40);
+
+    platinum.currentInstalledNib.cleanNib(false);
+    platinum.currentInstalledNib.polishNib("italic", true);
+    std::cout << "Currently installed nib type is: " << platinum.currentInstalledNib.style << std::endl;
+    platinum.currentInstalledNib.changeNib("bold");
+    std::cout << "Currently installed nib type is: " << platinum.currentInstalledNib.style << std::endl;
+
+    gbColor.currentGame.saveGameStateToRAM();
+    gbColor.currentGame.doYouHaveBattleToads();
+    gbColor.howBigIsScreen();
+
+    gbColor.adjustVolume(10.0f);
+    gbColor.adjustBrightness(35.0);
+    std::cout << "You are playing " << gbColor.currentGame.name << std::endl;
+    goldenSun.isClean = false;
+    gbColor.insertNewCartridge(goldenSun);
+    std::cout << "You are playing " << gbColor.currentGame.name << std::endl;
+    gbColor.currentGame.cleanCartridgeHead(gbColor.currentGame);
+    gbColor.currentGame.playTilLvl(99);
+
+    marioBros3.cleanCartridgeHead(marioBros3);
+    marioBros3.playTilLvl(5);
+    marioBros3.saveGameStateToRAM();
+    marioBros3.doYouHaveBattleToads();  
+
+    vanHalen.displayAlbumName();
+    phillips.playCD(2, vanHalen);
+    phillips.changeTrack (2, vanHalen);
+    phillips.playCdTillEnd(2);
+    std::cout << "Hey, what does this button do?...\n";
+    phillips.pausePlayback();
+
     std::cout << "good to go!" << std::endl;
 }
